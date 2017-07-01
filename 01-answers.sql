2bf2176ffde9739e1f7ba93c9da3a1bc2ffd1317
Abhay Kulkarni <developer.ak23@gmail.com>
2017-07-02 00:42:03 +0530

    Added the final sql

--- 01-answers.sql
+++ 01-answers.sql
@@ -1,4 +1,5 @@
-Ans1:
+--Q1: Find total sales for each product 
+--Ans1:
 select p.PRODUCT_NAME, sale
 FROM (
   select PRODUCT_ID, sum(quantity * price) as sale
@@ -10,8 +11,8 @@ order by sale desc;
 
 
 
-
-Ans2:
+--Q2: Find the product which does not have a sales at all
+--Ans2:
 -- using not exists clause
 SELECT PRODUCT_NAME, PRODUCT_ID
 FROM PRODUCTS p1
@@ -34,7 +35,9 @@ WHERE p.PRODUCT_ID NOT in (
 
 
 
-Ans3:
+
+--Q3: Write a SQL query to find the products whose sales decreased in 2012 compared to 2011? 
+--Ans3:
 SELECT P.PRODUCT_NAME
 FROM  PRODUCTS P,
       SALES S_2011,
@@ -44,4 +47,18 @@ AND S_2012.YEAR = 2012
 AND (S_2012.PRODUCT_ID * S_2012.Quantity) < (S_2011.PRODUCT_ID * S_2011.Quantity) -- could just check the quantity alone!
 AND S_2012.PRODUCT_ID = P.PRODUCT_ID
 AND S_2011.PRODUCT_ID = P.PRODUCT_ID
+
+
+
+--Q4: Write a query to select the top product sold in each year
+--Ans4:
+select p.PRODUCT_NAME, s.Quantity, s.year
+FROM (
+  select year, max(quantity) as quantity
+  from SALES
+  GROUP BY year
+) A
+INNER JOIN SALES s on (s.YEAR = A.year AND A.quantity = s.Quantity)
+INNER JOIN PRODUCTS p on p.PRODUCT_ID = s.PRODUCT_ID
+ORDER BY s.year desc;
 
