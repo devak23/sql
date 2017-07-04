d87cc567a13a6f90acb5f32b6732582d2b91c776
Abhay Kulkarni <developer.ak23@gmail.com>
2017-07-04 18:25:07 +0530

    Added more sql queries

--- 01-answers.sql
+++ 01-answers.sql
@@ -1,5 +1,5 @@
---Q1: Find total sales for each product 
---Ans1:
+-- Q1: Find total sales for each product
+-- Ans1:
 select p.PRODUCT_NAME, sale
 FROM (
   select PRODUCT_ID, sum(quantity * price) as sale
@@ -10,9 +10,8 @@ INNER JOIN PRODUCTS p ON p.PRODUCT_ID = result.PRODUCT_ID
 order by sale desc;
 
 
-
---Q2: Find the product which does not have a sales at all
---Ans2:
+-- Q2: Find the product which does not have a sales at all
+-- Ans2:
 -- using not exists clause
 SELECT PRODUCT_NAME, PRODUCT_ID
 FROM PRODUCTS p1
@@ -36,8 +35,8 @@ WHERE p.PRODUCT_ID NOT in (
 
 
 
---Q3: Write a SQL query to find the products whose sales decreased in 2012 compared to 2011? 
---Ans3:
+-- Q3: Write a SQL query to find the products whose sales decreased in 2012 compared to 2011?
+-- Ans3:
 SELECT P.PRODUCT_NAME
 FROM  PRODUCTS P,
       SALES S_2011,
@@ -46,12 +45,10 @@ WHERE S_2011.YEAR = 2011
 AND S_2012.YEAR = 2012
 AND (S_2012.PRODUCT_ID * S_2012.Quantity) < (S_2011.PRODUCT_ID * S_2011.Quantity) -- could just check the quantity alone!
 AND S_2012.PRODUCT_ID = P.PRODUCT_ID
-AND S_2011.PRODUCT_ID = P.PRODUCT_ID
+AND S_2011.PRODUCT_ID = P.PRODUCT_ID;
 
-
-
---Q4: Write a query to select the top product sold in each year
---Ans4:
+-- Q4: Write a query to select the top product sold in each year
+-- Ans4:
 select p.PRODUCT_NAME, s.Quantity, s.year
 FROM (
   select year, max(quantity) as quantity
@@ -61,4 +58,44 @@ FROM (
 INNER JOIN SALES s on (s.YEAR = A.year AND A.quantity = s.Quantity)
 INNER JOIN PRODUCTS p on p.PRODUCT_ID = s.PRODUCT_ID
 ORDER BY s.year desc;
+
+-- Q5: Write a query to find the average quantity of product sold so far
+-- Ans5:
+SELECT P.PRODUCT_NAME, AVG.AVG_QTY
+FROM (
+     SELECT PRODUCT_ID, sum(quantity)/count(QUANTITY) as AVG_QTY
+     FROM SALES
+     GROUP BY PRODUCT_ID
+) AVG
+INNER JOIN PRODUCTS P ON P.PRODUCT_ID = AVG.PRODUCT_ID;
+
+-- Q6: Write a query to find the products whose sold more than 10 quantities in a given year
+-- Ans6:
+SELECT p.PRODUCT_NAME, s.QUANTITY, s.YEAR
+FROM PRODUCTS p
+  INNER JOIN SALES s on p.PRODUCT_ID = s.PRODUCT_ID
+WHERE QUANTITY > 10;
+
+-- Q7: Write a query to find the products whose quantity sold in a year is greater than the average quantity of the product sold across all the years
+-- Ans7: my answer is the first query. It's not efficient but works
+SELECT p.PRODUCT_NAME, s.YEAR, s.QUANTITY
+FROM PRODUCTS p
+  INNER JOIN SALES s ON p.PRODUCT_ID = s.PRODUCT_ID
+  INNER JOIN (
+    SELECT s2.PRODUCT_ID, sum(QUANTITY)/count(QUANTITY) as AVG_QTY
+    FROM SALES s2
+    GROUP BY PRODUCT_ID
+) AVG on AVG.PRODUCT_ID = s.PRODUCT_ID
+WHERE s.QUANTITY > AVG.AVG_QTY;
+
+-- more efficient approach
+SELECT p.PRODUCT_NAME, s.YEAR, s.QUANTITY
+FROM PRODUCTS p
+INNER JOIN SALES s ON p.PRODUCT_ID = s.PRODUCT_ID
+WHERE s.QUANTITY > (
+  SELECT sum(QUANTITY)/count(QUANTITY)
+  FROM SALES s2
+  WHERE s2.PRODUCT_ID = s.PRODUCT_ID
+);
+
 
