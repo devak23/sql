ec3bc228be84cbc7220b26ea659f5b6776f72aa0
Abhay Kulkarni <developer.ak23@gmail.com>
2017-07-05 15:50:52 +0530

    Added more sql queries

--- 02-answers.sql
+++ 02-answers.sql
@@ -37,3 +37,27 @@ ORDER BY EXP_YRS ASC;
 SELECT EMPNO, ENAME, SAL, datediff(now(), hiredate)/(12 * 30) as EXP_YRS
 FROM EMP
 WHERE MGR = 7698;
+
+-- 11. Display all the details of the emps whose Comm. Is more than their Sal.
+SELECT * from EMP WHERE COMM > SAL;
+
+-- 12. List the emps in the asc order of Designations of those joined after the second half of 1981.
+SELECT * FROM EMP
+  WHERE HIREDATE > '1981-06-01'
+ORDER BY JOB ASC;
+
+-- 13. List the emps along with their Exp and Daily Sal is more than Rs.100
+SELECT e.*, datediff(NOW(), HIREDATE)/(12 * 30) as EXP_YRS, SAL/30 as DAILY_SAL
+FROM EMP e
+WHERE SAL/30 > 100;
+
+-- 14. List the emps who are either ‘CLERK’ or ‘ANALYST’ in the Desc order
+SELECT *
+FROM EMP
+WHERE JOB IN ('CLERK', 'ANALYST')
+ORDER BY JOB;
+
+-- 15. List the emps who joined on 1-MAY-81,3-DEC-81,17-DEC-81,19-JAN-80 in asc order of seniority
+SELECT * FROM EMP
+WHERE HIREDATE IN ('1981-05-01', '1981-12-03', '1981-12-17', '1980-01-19')
+ORDER BY HIREDATE ASC;
